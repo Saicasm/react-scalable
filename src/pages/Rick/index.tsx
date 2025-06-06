7502bf776b221bfb567add1b5d7b00fb940ebecb
Sai Kumar Nekkanti <saikumar.nekkanti@gwu.edu>
2025-06-06 16:01:49 -0400

    feat: card updates with TODO

--- src/pages/Rick/index.tsx
+++ src/pages/Rick/index.tsx
@@ -4,17 +4,18 @@ import Card from '../../components/Card/Card';
 
 const Rick: React.FC = () => {
     return (
-        <div className="text-center  my-16 mx-16 border-2 ">
-            <div className='h-20 w-full flex justify-evenly bg-light-bg-secondary items-center rounded-md border-2 border-cyan-950'>
+        <div className="text-center  my-16 mx-16 ">
+            <div className='h-20 w-full flex justify-evenly bg-light-bg-secondary items-center rounded-md border-2 '>
                 <span> All Characters</span>
                 <span>Search By name</span>
                 <span>Recents</span>
             </div>
-            <div className='mt-8 bg-light-bg-primary items-center rounded-md border-2 border-cyan-950'>
+            <div className='mt-8 bg-light-bg-primary items-center rounded-md flex gap-12'>
                 <Card />
-                Get all characters from the Rick and Morty API.
+                <Card />
+                <Card />
             </div>
-
+            Get all characters from the Rick and Morty API.
         </div>
     );
 };
